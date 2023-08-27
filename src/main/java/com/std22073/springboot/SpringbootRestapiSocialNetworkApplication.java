7b63c97017fc8ce0993d8ab456c618ef02206272
Antenaina2002 <hei.antenaina@gmail.com>
2023-08-27 11:54:44 +0200

    initialised springboot crud only project

---
+++ src/main/java/com/std22073/springboot/SpringbootRestapiSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.std22073.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestapiSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/std22073/springboot/SpringbootRestapiSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.std22073.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapiSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
