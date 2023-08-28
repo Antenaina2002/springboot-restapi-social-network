c15b8d759dc90699d3802c0017e4d900f675fbbe
Antenaina2002 <hei.antenaina@gmail.com>
2023-08-28 18:49:45 +0200

    feat: added models for publication files. Implemented fields and constructors.

---
+++ src/main/java/model/Publication.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.*;
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Publication {
+    private int id_publication;
+    private String contenu;
+    public Publication(int id) {
+        this.id_publication = id;
+    }
+    public Publication(int id, String content){
+        this.id_publication = id;
+        this.contenu = content;
+    }
+}
